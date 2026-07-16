0cbcd67964a1263c5f081c46456ee0b132d27549
Ronit0o0 <ronit.rathod@torontofilmschool.ca>
2026-07-16 10:49:52 -0400

    Completed the Press E to interact Coding gym challenge 1

--- Assets/Scripts/Coding Gym/Week 10/SpriteChanger.cs
+++ Assets/Scripts/Coding Gym/Week 10/SpriteChanger.cs
@@ -6,17 +6,12 @@ public class SpriteChanger : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public List<Sprite> sprites;
-    public InputAction action;
     int i = 0;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         spriteRenderer.sprite = sprites[0];
-        //this is also something new that allows me to choose a input for the context.performed to work
-        action.AddBinding("<Keyboard>/space");
-        //This allows the action to be performed when the input is detected
-        action.Enable();
     }
 
     // Update is called once per frame
@@ -27,12 +22,11 @@ public class SpriteChanger : MonoBehaviour
 
     public void ChangeSprite(InputAction.CallbackContext context)
     {
+        Debug.Log("Button Pressed");
         //I found that .performed makes the code work when a button is pressed compared to the other methods
-        if (context.performed)
+        if (context.started)
         {
-            Debug.Log("Space was pressed");
             i++;
-            
          
             if (i >= sprites.Count)
             {
