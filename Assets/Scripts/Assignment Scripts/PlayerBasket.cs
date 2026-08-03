57beb2d97b2b89f663872cc41bf88a4a5a465243
Ronit0o0 <ronit.rathod@torontofilmschool.ca>
2026-08-03 15:06:38 -0400

    Completed Movement Challenge for player with boundries and dash mechanic

--- Assets/Scripts/Assignment Scripts/PlayerBasket.cs
+++ Assets/Scripts/Assignment Scripts/PlayerBasket.cs
@@ -1,12 +1,17 @@
-
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerBasket : MonoBehaviour
 {
     //The speed at which the player will move at
-    public float speed;
+    public float speed = 5f;
     //This is a public variable of the players x and y vectors and moves with player input only 
     private Vector2 playerMovement = Vector2.zero;
+    //These variables are for the dash 
+    private float dashSpeed = 8f;
+    private float dashDuration = 0.5f;
+    private bool isDashing = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,11 +26,47 @@ public class PlayerBasket : MonoBehaviour
 
         //This moves the player in either direction with WASD and the variable
         transform.position += (Vector3)playerMoving * speed * Time.deltaTime;
+
+        //This will be the boundries on how far the player can move to the left and right of the screen
+        //This will make it so the player wont move past the threshold and will just stay at the Max x position for either left or rightof the screen
+        if(transform.position.x < -4)
+        {
+            transform.position = new Vector3(-4f, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x > 4f)
+        {
+            transform.position = new Vector3(4f, transform.position.y, transform.position.z);
+        }
     
     }
-
+    //This will read the input from the player and will allow them to move left and right
     public void OnMove(InputAction.CallbackContext context)
     {
         playerMovement = context.ReadValue<Vector2>();
+    }
+
+    //This function will allow the player to dash to a fruit and will increase their speed when the input is click and when released it will go back to the original speed
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        //This checks the bool within the Coroutine to see if the player is dashing or not and if the input was pressed then to start the Coroutine
+        if(context.performed && !isDashing)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    //This coroutiune allows my dash logic to work by having the player dash and then changing the speed to the original speed after the dash duration is over
+    // Reference Video: https://www.youtube.com/watch?v=KQGo-Am1DaA
+    IEnumerator Dash()
+    {
+        isDashing = true;
+
+        speed = dashSpeed;
+
+        yield return new WaitForSeconds(dashDuration);
+
+        speed = 5f;
+
+        isDashing = false;
     }
 }
